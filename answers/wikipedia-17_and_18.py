11433784120797ed58b366c57514ec5448aa9965
groceryheist <nathante@uw.edu>
2016-04-22 21:50:17 -0700

    rename files to simplify names. add answer to 17 and 18.

--- answers/wikipedia-17_and_18.py
+++ answers/wikipedia-17_and_18.py
@@ -16,23 +16,15 @@ agents = 'all-agents'
 page_title = 'Panama Papers'
 period = 'daily'
 start_date = '20160404'
-end_date = '20160420'
+end_date = '20160423'
 
 wp_call = requests.get(ENDPOINT + wp_code + '/' + access + '/' + agents + '/' + quote(page_title, safe='') + '/' + period + '/' + start_date + '/' + end_date)
 response = wp_call.json()
 
-max_days = []
-max_views = 0
+total_views = 0
+days = 23-4
 for item in response['items']:
-    views = item['views']
-    day = item['timestamp']
-    if views > max_views:
-        max_views = views
-        max_days = [day]
-    elif views == max_views:
-        max_days.append(day)
-    else:
-        pass
+    total_views = total_views + item['views']
 
-print(page_title + ' had the most views on ' + str(max_days))
-print('with ' + str(max_views) + ' views')
+views_per_day = total_views / days
+print(page_title + ' had ' + str(views_per_day) + ' views per day')
